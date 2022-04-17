6c75aecb8207f0a35ea76e70988d4b72bd1906f2
Ирина <vredprep@inbox.ru>
2022-04-17 22:30:14 +0300

    вспомогательный класс контактов

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+    def __init__(self, first, middle, nick, title, address, mobile, email):
+        self.email = email
+        self.first = first
+        self.middle = middle
+        self.nick = nick
+        self.title = title
+        self.address = address
+        self.mobile = mobile
+        
+        
