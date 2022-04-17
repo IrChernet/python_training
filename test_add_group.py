0b0948bade06aaad39e7487f0c9ce131d41134b2
Ирина <vredprep@inbox.ru>
2022-04-17 20:37:32 +0300

    передача параметров в методы

--- test_add_group.py
+++ test_add_group.py
@@ -19,17 +19,17 @@ class TestAddGroup(unittest.TestCase):
     def submit_create(self, wd):
         wd.find_element_by_name("submit").click()
 
-    def fill_group_form(self, wd):
+    def fill_group_form(self, wd, name, header, footer):
         # fill form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new group1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("my new")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("comment gropp")
+        wd.find_element_by_name("group_footer").send_keys(footer)
 
     def init_new_group(self, wd):
         wd.find_element_by_name("new").click()
@@ -37,12 +37,12 @@ class TestAddGroup(unittest.TestCase):
     def open_page_group(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, passw):
         # login
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(passw)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
@@ -51,10 +51,10 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", passw='secret')
         self.open_page_group(wd)
         self.init_new_group(wd)
-        self.fill_group_form(wd)
+        self.fill_group_form(wd, name="group1", header= "my new", footer= "comment group")
         self.submit_create(wd)
         self.return_to_group_page(wd)
         self.logout(wd)
