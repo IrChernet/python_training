17447021ef2a4bf0576640421babb7a74e4b84e3
Ирина <vredprep@inbox.ru>
2022-04-30 21:13:34 +0500

    редактирование группы

--- fixture/group.py
+++ fixture/group.py
@@ -43,6 +43,24 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def edit_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        # select 1 group
+        wd.find_element_by_name("selected[]").click()
+        # submit edit
+        wd.find_element_by_name("edit").click()
+        # new elements
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("New n")
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("new h")
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("new f")
+        # submit changes
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
 
 
 
---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+def test_edit_group(app):
+    app.session.login(username="admin", passw="secret")
+    app.group.edit_group()
+    app.session.logout()
