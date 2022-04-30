94b89919b8e46f117d4885aa6702a43cf5ad7eef
Ирина <vredprep@inbox.ru>
2022-04-30 20:25:43 +0500

    общая фикстура

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-
 from fixture.application import Application
 
 @pytest.fixture(scope= "session")
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-from model.group import Group
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    # create fixture
-    fixture = Application()
-    # destroy fixture
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_del_1group(app):
+    app.session.login(username="admin", passw="secret")
     app.group.delete_1group()
     app.session.logout()
