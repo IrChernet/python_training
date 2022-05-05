7a5db4e891562bc18a719593d81533f88393b9ec
Ирина <vredprep@inbox.ru>
2022-05-05 13:37:23 +0500

    изменение теста модификации первого контакта

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,9 +2,18 @@
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
+def test_edit_first_in_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first='new test'))
     app.contact.edit_first(Contact(first='new F'))
+
+def test_edit_middle_in_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first='new test'))
     app.contact.edit_first(Contact(middle='new MM'))
+
+def test_edit_last_in_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first='new test'))
     app.contact.edit_first(Contact(last="New last"))
+
