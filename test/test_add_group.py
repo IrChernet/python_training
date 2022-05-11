5cffbad2618de5416a0f363770851e013e6e93c9
Ирина <vredprep@inbox.ru>
2022-05-11 13:22:46 +0500

    получение списка групп с ошибкой

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,11 @@ from model.group import Group
 
 
 def test_add_group1(app):
+    old_groups = app.group.get_group_list
     app.group.create(Group(name="my1", header="hed1", footer="footer1"))
+    new_groups = app.group.get_group_list
+    assert len(old_groups) + 1 == len(new_groups)
 
 
-def test_add_group_empty(app):
-    app.group.create(Group(name="", header="", footer=""))
+#def test_add_group_empty(app):
+#    app.group.create(Group(name="", header="", footer=""))
