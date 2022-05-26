ecdd42e3e3d8ad1cfd8cc494fb2dc75931899118
Ирина <vredprep@inbox.ru>
2022-05-26 16:15:15 +0500

    передача параметров в тесты при запуске

--- conftest.py
+++ conftest.py
@@ -1,22 +1,27 @@
+
 import pytest
+#import json
 from fixture.application import Application
 
 
 fixture = None
-
+#target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-    # create fixture
-        fixture = Application()
-        fixture.session.login(username="admin", passw='secret')
-
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", passw='secret')
+#    global target
+    if fixture is None or not fixture.is_valid():
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username="admin", passw="secret")
+#    if target is None:
+#        with open(request.config.getoption("--target")) as config_file:
+#            target = json.load(config_file)
+#    if fixture is None or not fixture.is_valid():
+#        fixture = Application(browser=browser, base_url=target['baseUrl'])
+#    fixture.session.ensure_login(username=target["username"], passw=target["password"])
     return fixture
 
 
@@ -27,3 +32,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+#    parser.addoption("--target", action="store", default="target.json")
--- fixture/application.py
+++ fixture/application.py
@@ -7,12 +7,20 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("unrecognised browser %s", browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -20,6 +28,10 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
