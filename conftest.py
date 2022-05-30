66dae48e1ef5e38d091627fb5eee8c2517f73823
Ирина <vredprep@inbox.ru>
2022-05-30 12:00:01 +0500

    конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,27 +1,24 @@
 
 import pytest
-#import json
+import json
 from fixture.application import Application
 
 
 fixture = None
-#target = None
+target = None
+
 
 @pytest.fixture
 def app(request):
     global fixture
-#    global target
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file1:
+            target = json.load(config_file1)
     if fixture is None or not fixture.is_valid():
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", passw="secret")
-#    if target is None:
-#        with open(request.config.getoption("--target")) as config_file:
-#            target = json.load(config_file)
-#    if fixture is None or not fixture.is_valid():
-#        fixture = Application(browser=browser, base_url=target['baseUrl'])
-#    fixture.session.ensure_login(username=target["username"], passw=target["password"])
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target["username"], passw=target["password"])
     return fixture
 
 
@@ -33,7 +30,8 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-#    parser.addoption("--target", action="store", default="target.json")
+#    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,8 +12,8 @@ def random_full_string(prefix, maxlen):
 
 
 def random_phone_string(prefix, maxlen):
-    sumbol = "+" + string.digits + "-"
-    return prefix + "".join([random.choice(sumbol) for i in range(random.randrange(maxlen))])
+    sumbol = "+7(" + string.digits*3 + ")" + string.digits*3
+    return prefix + "".join([random.choice(sumbol) for i in range(10)])
 
 
 def random_mail_string(prefix, maxlen):
@@ -22,10 +22,10 @@ def random_mail_string(prefix, maxlen):
 
 
 testdata = [
-    Contact(first=first, last="last", address="address", email="email", mobile="mobile", home="home")
+    Contact(first=first, last=last, address=address, email=email, mobile=mobile, home=home)
     for first in ["", random_full_string("first", 10)]
     for last in ["", random_full_string("last", 15)]
-   for address in ["", random_full_string("address", 30)]
+    for address in ["", random_full_string("address", 30)]
     for email in ["", random_mail_string("email", 15)]
     for mobile in ["", random_phone_string("mobile", 10)]
     for home in ["", random_phone_string("home", 10)]
