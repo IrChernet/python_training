6a1227f370911099df59057e6f9805fe5ea766d9
Ирина <vredprep@inbox.ru>
2022-05-30 14:35:05 +0500

    еще один способ параметризации - динамическая генерация

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+from model.contact import Contact
+
+testdate = [
+    Contact(first="first1", last="last1", address="address1", email="email1", mobile="mobile1", home="home1"),
+    Contact(first="first2", last="last2", address="address2", email="email2", mobile="mobile2", home="home2")
+]
