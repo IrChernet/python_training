dedf5e6cf06a8d85cceff0149bf12b0d7b9e6fa4
Ирина <vredprep@inbox.ru>
2022-06-03 13:02:49 +0500

    проверка списков получаемых из БД в тесте редактирования групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,16 +2,16 @@
 from model.group import Group
 from random import randrange
 
-def test_edit_some_group_name(app):
-    if app.group.count() == 0:
+def test_edit_some_group_name(app, db):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test group"))
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     group = Group(name="new group")
-    group.id = old_groups[index].id
+    group.id_gr = old_groups[index].id
     app.group.edit_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == app.group.count()
+    new_groups = db.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
