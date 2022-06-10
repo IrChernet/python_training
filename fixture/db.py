c201a99e36d29b76abf15581e90240aa6e310e2d
Ирина <vredprep@inbox.ru>
2022-06-10 14:08:48 +0500

    тест проверки информации о контактах с телефонами на главной с бд

--- fixture/db.py
+++ fixture/db.py
@@ -29,13 +29,15 @@ class DbFixture:
         list_cont = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address from addressbook where deprecated= '0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, email, email2, email3, home, work, mobile, phone2 from addressbook where deprecated= '0000-00-00 00:00:00'")
+#        ("select id, firstname, lastname, address from addressbook where deprecated= '0000-00-00 00:00:00'")
             for row in cursor:
-                (id, first, last, address) = row
-#                  email, email2, email3, home, mobile, phone2) = row
-                list_cont.append(Contact(id_cont=str(id), first=first, last=last, address=address))
-#                list_cont.append(Contact(id_cont=str(id), first=first, last=last, address=address,
-#                all_mail=all_mail, all_phones=all_phone))
+                (id, first, last, address, email, email2, email3, home, work, mobile, phone2) = row
+                list_cont.append(Contact(id_cont=str(id), first=first, last=last, address=address,
+                 home=home, mobile=mobile, work=work,  secondphone=phone2,
+                           email=email, email2=email2, email3=email3))
+#                (id, first, last, address) = row
+#            list_cont.append(Contact(id_cont=str(id), first=first, last=last, address=address))
         finally:
             cursor.close()
         return list_cont
