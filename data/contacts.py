14dad5d32bbecc3ddf809eb1dda5a836ce59977c
Ирина <vredprep@inbox.ru>
2022-07-28 21:46:02 +0500

    19- загрузка тестовых данных из файлов, доработка

--- data/contacts.py
+++ data/contacts.py
@@ -1,6 +1,6 @@
 from model.contact import Contact
 
-testdate = [
+testdata = [
     Contact(first="first1", last="last1", address="address1", email="email1", mobile="mobile1", home="home1"),
     Contact(first="first2", last="last2", address="address2", email="email2", mobile="mobile2", home="home2")
 ]
